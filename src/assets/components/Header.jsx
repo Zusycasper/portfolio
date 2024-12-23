d0c4909bc0d933efd832f472be1877702c147e11
ZusyCasper <ZusyCasper@gmail.com>
2024-12-23 14:52:45 +0530

    pages added

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+    <div className="sticky top-0 z-50 bg-white shadow-md">
+      <div className="flex items-center justify-between p-4">
+        
+        </div>
+    </div>
+    </>
+  );
+}
+
+export default Header;
